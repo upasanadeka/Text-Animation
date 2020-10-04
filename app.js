1638603ed9c1762faed18bcca01728a1e865332c
Swapnil Tiwari <swapnil_tiwari@outlook.com>
2020-10-04 15:12:57 +0530

    added batt status

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const logo = document.querySelectorAll("#logo path");
 
 console.log(logo);
-
+battstatus=async ()=>{
+    stat= await window.navigator.getBattery()
+    console.log(stat)
+}
+battstatus()
 for(i = 0; i<logo.length; i++) {
     console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
 }
